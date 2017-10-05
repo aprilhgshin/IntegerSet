ad0ce2a20a6426a9b234db90e5f0922375dc1d46
aprilhjshin <31872545+aprilhjshin@users.noreply.github.com>
2017-10-05 01:21:56 -0700

    Add files via upload

--- IntegerSet.cpp
+++ IntegerSet.cpp
@@ -28,8 +28,8 @@ IntegerSet::IntegerSet(int *array, int arraySize)
 IntegerSet* IntegerSet::unionOfSets(IntegerSet *anotherSet) const
 {
 	IntegerSet *combinedUSet;							  /* a pointer to an IntegerSet, which I will 
-														     then use the pointer to dynamically allocate 
-															 an object of the IntegerSet class */
+														then use the pointer to dynamically allocate 
+													an object of the IntegerSet class */
 	combinedUSet = new IntegerSet(anotherSet->setSize);	  // size for each instance can be different
 
 	for (int counter = 0; counter < setSize; ++counter)
